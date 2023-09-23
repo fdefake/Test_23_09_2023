9f6e22d06f3f04e96381a5a366f676bf4ead39c7
fdefake <fdefake80@gmail.com>
2023-09-23 14:39:08 -0700

    First

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript para la interactividad de la página
+
+// Ejemplo: Cambiar el color del encabezado cuando se hace clic en él
+const header = document.querySelector("header");
+header.addEventListener("click", function () {
+  header.style.backgroundColor = "red";
+});
+
+// Más código JavaScript puede agregarse aquí para interactuar con la página web.
